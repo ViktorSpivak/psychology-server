489d0a53e9118c4f30308fbe431a89de9bb34004
ViktorSpivak <49995904+ViktorSpivak@users.noreply.github.com>
2020-07-27 00:57:23 +0300

    fix joi validation

--- controllers/validation.js
+++ controllers/validation.js
@@ -4,13 +4,13 @@ class Validations {
   validateRequest = (req, res, next) => {
     const rules = Joi.object({
       name: Joi.string().required(),
-      email: Joi.string().required(),
       phone: Joi.string().required(),
+      email: Joi.string().required(),
       text: Joi.string().required(),
     });
     const validationResult = rules.validate(req.body);
     if (validationResult.error) {
-      return res.status(421).json({ message: "Missing required field" });
+      return res.status(422).json({ message: "Missing required field" });
     }
     next();
   };
--- routing/requestHandler.js
+++ routing/requestHandler.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/user_controllers");
 const validation = require("../controllers/validation");
 
-router.get("/request", validation.validateRequest, userController.createUser);
+router.post("/request", validation.validateRequest, userController.createUser);
 router.get(
   "/test",
   //  validation.validateRequest,
