76175f209f4406d236bfedd0a1f0baf55bc841aa
ViktorSpivak <49995904+ViktorSpivak@users.noreply.github.com>
2020-08-04 18:16:37 +0300

    sendgred fix cors

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ module.exports = class myMongoDBServer {
     this.server.use(express.urlencoded({ extended: true }));
     this.server.use(cors());
     this.server.use(morgan("combined"));
+    this.server.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*"); // Change later to only allow our server
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      next();
+    });
   };
   initRoutes = () => {
     this.server.use("/", routing);
@@ -40,7 +52,7 @@ module.exports = class myMongoDBServer {
   };
   initErrorHandler = () => {
     this.server.use((err, req, res, next) => {
-      console.error("Error:", err.message);
+      console.error("ErrorHandler:", err.message);
       res.status(500).send("Something broke!");
     });
   };
